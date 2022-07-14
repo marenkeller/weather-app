0cf4bae5da32b2922a76f593d429c95c9b3b0e3a
Maren Keller <104692152+marenkeller@users.noreply.github.com>
2022-07-14 15:33:05 +0200

    Updated JS and the corresponding HTML and CSS
    Added Celcius/Fahrenheit conversion, adjusted the displayed time+date, added the API for the main weather icon.

--- java.js
+++ java.js
@@ -1,46 +1,39 @@
-function formatdate(date) {
-  let hours = date.getHours();
+function formatDate(date) {
+  let hours = now.getHours();
   if (hours < 10) {
-    hours = `0${hours};`;
+    hours = `0${hours}`;
   }
-  let minutes = date.getMinutes();
+  let minutes = now.getMinutes();
   if (minutes < 10) {
-    minutes = `0${minutes};`;
+    minutes = `0${minutes}`;
   }
-  let days = ["sun", "mon", "tues", "wed", "thurs", "fri", "sat"];
-  let day = days[date.getDay()];
-  let dateOfMonth = date.getDate();
-  let months = [
-    "jan",
-    "feb",
-    "mar",
-    "apr",
-    "may",
-    "jun",
-    "jul",
-    "aug",
-    "sep",
-    "oct",
-    "nov",
-    "dec",
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
   ];
-
-  let month = months[date.getMonth()];
-  return `${day}, ${month} ${dateOfMonth}  ${hours}:${minutes}`;
+  let day = days[now.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
 
-let currentTime = document.querySelector("#hourDate");
+let currentTime = document.querySelector("#date");
 let now = new Date();
-currentTime.innerHTML = formatdate(now);
+currentTime.innerHTML = formatDate(now);
 
-function displayWeatherCondition(response) {
-  document.querySelector("#city-h1").innerHTML = response.data.name;
-  document.querySelector("#temperatureMain").innerHTML = Math.round(
-    response.data.main.temp
-  );
+function showWeather(response) {
+  document.querySelector("#city-input").innerHTML = response.data.name;
+
+  celciusTemp = response.data.main.temp;
+
+  document.querySelector("#temperature-main").innerHTML =
+    Math.round(celciusTemp);
 
   document.querySelector("#description").innerHTML =
-    response.data.weather[0].main;
+    response.data.weather[0].description;
   document.querySelector("#feels-like").innerHTML = Math.round(
     response.data.main.feels_like
   );
@@ -51,40 +44,64 @@ function displayWeatherCondition(response) {
     response.data.main.temp_min
   );
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-  //document.querySelector("#iconMain").innerHTML = response.data.weather.icon;
+
+  let mainIconElement = document.querySelector("#main-icon");
+  mainIconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  mainIconElement.setAttribute("alt", response.data.weather[0].description);
+
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
 }
 
-function searchCity(city) {
+function search(city) {
   let apiKey = "92b6fb38291bebfb68bbc438f53dc255";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayWeatherCondition);
+  axios.get(apiUrl).then(showWeather);
 }
 
 function handleSubmit(event) {
   event.preventDefault();
-  let city = document.querySelector("#city-input").value;
-  searchCity(city);
+  let cityInput = document.querySelector("#city-input");
+  search(cityInput.value);
 }
 
-function searchLocation(position) {
-  let apiKey = "92b6fb38291bebfb68bbc438f53dc255";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+function showFahrenheit(event) {
+  event.preventDefault();
+  let fahrenheitTemp = (celciusTemp * 9) / 5 + 32;
+  celcius.classList.remove("active");
+  fahrenheit.classList.add("active");
+  let fahrenheitElement = document.querySelector("#temperature-main");
+  fahrenheitElement.innerHTML = Math.round(fahrenheitTemp);
+}
 
-  axios.get(apiUrl).then(displayWeatherCondition);
+function showcelcius(event) {
+  event.preventDefault();
+  celcius.classList.add("active");
+  fahrenheit.classList.remove("active");
+  let tempElement = document.querySelector("#temperature-main");
+  tempElement.innerHTML = Math.round(celciusTemp);
 }
 
 function getCurrentLocation(event) {
   event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchLocation);
+  navigator.geolocation.getCurrentPosition(search);
 }
 
-let searchForm = document.querySelector("#search-bar");
-searchForm.addEventListener("submit", handleSubmit);
+let celciusTemp = null;
+
+let fahrenheit = document.querySelector("#fahrenheit");
+fahrenheit.addEventListener("click", showFahrenheit);
+
+let celcius = document.querySelector("#celcius");
+celcius.addEventListener("click", showcelcius);
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
 
 let currentLocationButton = document.querySelector("#current-city-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
-
-searchCity("Oslo");
+search("Oslo");
